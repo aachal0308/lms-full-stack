0a4d7012615a0af607c12478deae6cc670ae746b
aachal0308 <aachalladdha03@gmail.com>
2025-07-13 20:42:25 +0530

    Remove server.js after converting to Vercel handler

--- server/app.js
+++ server/app.js
@@ -28,9 +28,4 @@ app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
 
-// Port
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+export default app
